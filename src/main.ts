7b12933622f8837b0de3a9fb1cbc9e9f1e7139a9
sedice <1107238436@qq.com>
2022-01-05 16:09:25 +0800

    添加 vue-router pinia reset.css 支持

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
+import router from "./router";
+import "./assets/styles/index.less";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/home/index.vue";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
