f0a3c8bdfc5174bb6fe606b1a29c6c1120260af0
sedice <1107238436@qq.com>
2022-01-05 16:19:34 +0800

    调整全局样式位置

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import router from "./router";
-import "./assets/styles/index.less";
 import App from "./App.vue";
+
+// 全局样式
+import "./assets/styles/index.less";
 
 const app = createApp(App);
 app.use(router);
