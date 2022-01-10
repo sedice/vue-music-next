557846a19e9fd2579eb2b61e1d0b611c0382e77e
sedice <1107238436@qq.com>
2022-01-10 15:58:01 +0800

    引用轮播组件

--- src/views/recommended/index.vue
+++ src/views/recommended/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>recommended</div>
+  <div class="slider-wrapper">
+    <Slider v-if="reactiveSliders.length > 0" :sliders="reactiveSliders" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Slider from "@/components/base/slider/slider.vue";
+import { getRecommend, IRecommendedResult } from "@/service/recommended";
+
+const reactiveSliders = ref<IRecommendedResult["sliders"]>([]);
+
+onMounted(async () => {
+  const { sliders } = await getRecommend();
+  reactiveSliders.value = sliders;
+});
+</script>
+
+<style lang="scss" scoped>
+.slider-wrapper {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
