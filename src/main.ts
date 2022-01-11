35f762198d67db8c783de6978176faa4ebe6e4e5
sedice <1107238436@qq.com>
2022-01-11 16:59:49 +0800

    使用图片懒加载

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
+import LazyLoad from "vue3-lazy";
+import DefaultImg from "@/assets/images/default.png";
 
 // 全局样式
 import "@/assets/scss/index.scss";
 
 const app = createApp(App);
-app.use(router);
+app.use(router).use(LazyLoad, {
+  loading: DefaultImg,
+});
 app.mount("#app");
--- src/views/recommended/index.vue
+++ src/views/recommended/index.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="slider-wrapper">
-    <Slider v-if="sliders.length > 0" :sliders="sliders" />
-  </div>
-  <div class="recommend-list">
-    <h1 v-show="!loading" class="list-title">热门歌单推荐</h1>
-    <ul>
-      <li v-for="item in albums" :key="item.id" class="item">
-        <div class="icon">
-          <img :src="item.pic" width="60" height="60" />
+  <div class="recommended">
+    <Scroll class="scroll-content">
+      <div>
+        <div class="slider-wrapper">
+          <Slider v-if="sliders.length > 0" :sliders="sliders" />
         </div>
-        <div class="text">
-          <h2 class="name">{{ item.username }}</h2>
-          <p class="title">{{ item.title }}</p>
+        <div class="recommend-list">
+          <h1 v-show="!loading" class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in albums" :key="item.id" class="item">
+              <div class="icon">
+                <img v-lazy="item.pic" width="60" height="60" />
+              </div>
+              <div class="text">
+                <h2 class="name">{{ item.username }}</h2>
+                <p class="title">{{ item.title }}</p>
+              </div>
+            </li>
+          </ul>
         </div>
-      </li>
-    </ul>
+      </div>
+    </Scroll>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, reactive, toRefs } from "vue";
 import Slider from "@/components/base/slider/slider.vue";
+import Scroll from "@/components/base/scroll/scroll.vue";
 import { getRecommend, IRecommendedResult } from "@/service/recommended";
 
 const reactiveResult = reactive<IRecommendedResult>({
@@ -39,43 +46,47 @@ const { sliders, albums } = toRefs(reactiveResult);
 </script>
 
 <style lang="scss" scoped>
-.slider-wrapper {
-  margin-top: 10px;
-}
-
-.recommend-list {
-  .list-title {
-    height: 65px;
-    line-height: 65px;
-    text-align: center;
-    font-size: $font-size-medium;
-    color: $color-theme;
-  }
-  .item {
-    display: flex;
-    box-sizing: border-box;
-    align-items: center;
-    padding: 0 20px 20px 20px;
-    .icon {
-      flex: 0 0 60px;
-      width: 60px;
-      padding-right: 20px;
-    }
-    .text {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      flex: 1;
-      line-height: 20px;
-      overflow: hidden;
-      font-size: $color-text;
-    }
-    .name {
-      margin-bottom: 10px;
-      color: $color-text;
-    }
-    .title {
-      color: $color-text-d;
+.recommended {
+  height: calc(100vh - 87px);
+  .scroll-content {
+    height: 100%;
+    overflow: hidden;
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 10px 20px;
+        /* border: 1px solid red; */
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $color-text;
+        }
+        .name {
+          margin-bottom: 10px;
+          color: $color-text;
+        }
+        .title {
+          color: $color-text-d;
+        }
+      }
     }
   }
 }
