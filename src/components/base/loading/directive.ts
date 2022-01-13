a046f9ca6e64ca06f4e35073f8e3951fcffd1a31
sedice <1107238436@qq.com>
2022-01-13 15:19:48 +0800

    添加ts提示

--- src/components/base/loading/directive.ts
+++ src/components/base/loading/directive.ts
@@ -4,16 +4,17 @@ import { addClass, removeClass } from "@/utils";
 
 const relativeCls = "v-loading-wrapper";
 
-interface LoadingContainer extends HTMLElement {
-  instance: any;
-}
+type LoadingInstance = InstanceType<typeof Loading>;
+type LoadingContainer = HTMLElement & {
+  instance: LoadingInstance;
+};
 
 const loadingDirective: Directive<HTMLElement, boolean> = {
   mounted(_el, { value, arg }) {
     const el = _el as LoadingContainer;
     const app = createApp(Loading);
     const instance = app.mount(document.createElement("div"));
-    el.instance = instance;
+    el.instance = instance as unknown as LoadingInstance;
     const title = arg;
     if (typeof title !== "undefined") {
       el.instance.setTitle(title);
